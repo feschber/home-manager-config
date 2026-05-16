5f627006637c7b0efea6faf353ac5aa33326dfb8
Ferdinand Schober <ferdinand.schober@fau.de>
2026-05-16 15:50:05 +0200

    fix desktop entry

--- desktop-entries.nix
+++ desktop-entries.nix
@@ -1,10 +1,12 @@
 { ... }:
 {
-  boot_windows = {
-    name = "Boot Windows";
-    genericName = "Boot Windows";
-    exec = "bash -c 'efibootmgr -n 2; reboot'";
-    terminal = false;
-    categories = [ "Utilities" ];
+  xdg.desktopEntries = {
+    boot_windows = {
+      name = "Boot Windows";
+      genericName = "Boot Windows";
+      exec = "bash -c 'efibootmgr -n 2; reboot'";
+      terminal = false;
+      categories = [ "Utilities" ];
+    };
   };
 }
