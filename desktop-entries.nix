d0b15be2f2d45b49b8804825b339f61a22600729
Ferdinand Schober <ferdinand.schober@fau.de>
2026-05-16 15:48:05 +0200

    add boot-windows desktop entry

---
+++ desktop-entries.nix
@@ -0,0 +1,10 @@
+{}:
+{
+  boot_windows = {
+    name = "Boot Windows";
+    genericName = "Boot Windows";
+    exec = "bash -c 'efibootmgr -n 2; reboot'";
+    terminal = false;
+    categories = [ "Utilities" ];
+  };
+}
--- home.nix
+++ home.nix
@@ -6,6 +6,7 @@
     ./fonts.nix
     ./starship.nix
     ./zsh.nix
+    ./desktop-entries.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
