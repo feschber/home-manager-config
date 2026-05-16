4174452bd217cf1ece3928f7be22a91c5abb6faf
Ferdinand Schober <ferdinand.schober@fau.de>
2026-05-16 16:00:42 +0200

    use absolute path to efibootmgr for sudo rights

--- desktop-entries.nix
+++ desktop-entries.nix
@@ -4,7 +4,7 @@
     boot_windows = {
       name = "Boot Windows";
       genericName = "Boot Windows";
-      exec = "${pkgs.bash}/bin/bash -c \"efibootmgr -n 2; reboot\"";
+      exec = "${pkgs.bash}/bin/bash -c \"${pkgs.efibootmgr}/bin/efibootmgr -n 2; reboot\"";
       terminal = false;
       categories = [ "Utility" ];
     };
