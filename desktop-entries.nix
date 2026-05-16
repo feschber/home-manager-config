7b03e33dc375ba80cca83387d969d12f15b42675
Ferdinand Schober <ferdinand.schober@fau.de>
2026-05-16 15:52:56 +0200

    try fixing command

--- desktop-entries.nix
+++ desktop-entries.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   xdg.desktopEntries = {
     boot_windows = {
       name = "Boot Windows";
       genericName = "Boot Windows";
-      exec = "bash -c 'efibootmgr -n 2; reboot'";
+      exec = "${pkgs.bash}/bin/bash -c \"efibootmgr -n 2; reboot\"";
       terminal = false;
       categories = [ "Utilities" ];
     };
